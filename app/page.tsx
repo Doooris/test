97c6be98df9692703e4fe7d329231f472481f0df
Doooris <hxpt2031@gmail.com>
2026-05-11 11:51:31 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,11 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            我的第一个 Vercel 自动部署网站.哈哈通啦！
+            我的第一个 Vercel 自动部署网站。哈哈通啦！
+            <br />
+            下一步绑定域名
           </h1>
+       
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
